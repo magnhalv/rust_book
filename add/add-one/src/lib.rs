3971ac369ccf734f67f7ea334a981765f50daac6
Magnus Halvorsen <magnus.halvorsen@acando.no>
2021-04-05 16:51:18 +0200

    Cargo workspaces

---
+++ add/add-one/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn add_one_should_add_one() {
+        assert_eq!(add_one(5), 6);
+    }
+}
---
+++ add/adder/src/main.rs
@@ -0,0 +1,5 @@
+use add_one;
+
+fn main() {
+    println!("Hello, world! Add one to 5 = {}", add_one::add_one(5));
+}
